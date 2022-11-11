8ee090f30886828e638d29fecd8bac573c5d32d0
Raghu K <raghuk.vit@gmail.com>
2022-11-11 07:26:35 +0530

    Update roboshop-mutable-infra/

--- main.tf
+++ main.tf
@@ -9,8 +9,13 @@ module "docdb" {
   source  = "./vendor/modules/docdb"
   docdb   = var.docdb
   env     = var.env
-  subnets = local.database_private_subnets[*].id
+  subnets = local.database_private_subnets["*"].id
 }
 
-
+module "rds" {
+  source  = "./vendor/modules/rds"
+  rds     = var.rds
+  env     = var.env
+  subnets = local.database_private_subnets["*"].id
+}
 
