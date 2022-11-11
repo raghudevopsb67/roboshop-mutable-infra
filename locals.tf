50cd182abaf8405e3069d86b23c2f07d34e8e721
Raghu K <raghuk.vit@gmail.com>
2022-11-11 06:36:52 +0530

    Update roboshop-mutable-infra/

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  private_subnets = tomap({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets })
+  private_subnets = { for k, v in module.vpc.private_subnets : "subnets" => v.subnets }
 }
 
