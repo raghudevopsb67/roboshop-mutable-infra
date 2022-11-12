0e968672608893074fa345c86e874b12c3b380bd
Raghu K <raghuk.vit@gmail.com>
2022-11-12 07:27:13 +0530

    Update roboshop-mutable-infra/

--- main.tf
+++ main.tf
@@ -27,5 +27,5 @@ module "vpc" {
 //
 
 output "app_subnets" {
-  value = lookup(module.vpc, "private_subnets", null)
+  value = lookup(lookup(lookup(module.vpc, "private_subnets", null), "app", null), "subnets", null)[*].id
 }
