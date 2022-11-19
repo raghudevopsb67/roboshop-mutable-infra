8f9525eb6dffbb77f6ff8f3e6b48bfd20b5d8612
Raghu K <raghuk.vit@gmail.com>
2022-11-19 06:25:29 +0530

    Update roboshop-mutable-infra/

--- main.tf
+++ main.tf
@@ -12,12 +12,14 @@ module "vpc" {
 }
 
 module "docdb" {
-  for_each = var.docdb
-  source   = "./vendor/modules/docdb"
-  name     = each.key
-  engine   = each.value.engine
-  env      = var.env
-  subnets  = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+  for_each            = var.docdb
+  source              = "./vendor/modules/docdb"
+  name                = each.key
+  engine              = each.value.engine
+  env                 = var.env
+  subnets             = flatten([for i, j in module.vpc : j.private_subnets["database"]["subnets"][*].id])
+  nodes               = each.value.nodes
+  skip_final_snapshot = each.value.skip_final_snapshot
 }
 
 module "rds" {
